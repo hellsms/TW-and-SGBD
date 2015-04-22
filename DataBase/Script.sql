c914379040b16ca10664239ab72f29a29b4b80dc
alis-andriescu <alis-andriescu@users.noreply.github.com>
2015-04-22 18:22:13 +0300

    Update Script.sql

--- DataBase/Script.sql
+++ DataBase/Script.sql
@@ -4,7 +4,7 @@ DROP TABLE Customers cascade constraints;
 
 CREATE TABLE Customers (
 
-customer_id NUMBER(3) PRIMARY KEY,
+customer_id NUMBER(9) PRIMARY KEY,
 
 organisation_or_person CHAR(20) NOT NULL,
 
@@ -48,9 +48,9 @@ DROP TABLE Ref_Product_Types cascade constraints;
 
 CREATE TABLE Ref_Product_Types (
 
-product_type_code NUMBER(3) PRIMARY KEY,
+product_type_code NUMBER(9) PRIMARY KEY,
 
-parent_product_type_code NUMBER(3) REFERENCES Ref_Product_Types(product_type_code),
+parent_product_type_code NUMBER(9) REFERENCES Ref_Product_Types(product_type_code),
 
 product_type_description CHAR(20) NOT NULL ) ;
 
@@ -62,11 +62,11 @@ DROP TABLE Products cascade constraints;
 
 CREATE TABLE Products (
 
-product_id NUMBER(3) PRIMARY KEY,
+product_id NUMBER(9) PRIMARY KEY,
 
-product_type_code NUMBER(3) REFERENCES Ref_product_Types (product_type_code),
+product_type_code NUMBER(9) REFERENCES Ref_product_Types (product_type_code),
 
-ret_merchandise_auth_nr NUMBER(3) NOT NULL,
+ret_merchandise_auth_nr NUMBER(9) NOT NULL,
 
 product_name CHAR(20) NOT NULL,
 
@@ -88,7 +88,7 @@ DROP TABLE Ref_Payment_Methods cascade constraints;
 
 CREATE TABLE Ref_Payment_Methods (
 
-payment_method_code NUMBER(3) PRIMARY KEY,
+payment_method_code NUMBER(9) PRIMARY KEY,
 
 payment_method_description CHAR(30) NOT NULL);
 
@@ -100,11 +100,11 @@ DROP TABLE Customer_Payment_Methods cascade constraints;
 
 CREATE TABLE Customer_Payment_Methods (
 
-customer_payment_id NUMBER(3) PRIMARY KEY,
+customer_payment_id NUMBER(9) PRIMARY KEY,
 
-customer_id NUMBER(3) REFERENCES Customers (customer_id),
+customer_id NUMBER(9) REFERENCES Customers (customer_id),
 
-payment_method_code NUMBER(3) REFERENCES Ref_Payment_Methods (payment_method_code),
+payment_method_code NUMBER(9) REFERENCES Ref_Payment_Methods (payment_method_code),
 
 credit_card_number NUMBER(5) ,
 
@@ -118,7 +118,7 @@ DROP TABLE Ref_Order_Status_Codes cascade constraints;
 
 CREATE TABLE Ref_Order_Status_Codes (
 
-order_status_code NUMBER(3) PRIMARY KEY,
+order_status_code NUMBER(9) PRIMARY KEY,
 
 order_status_description CHAR(30) NOT NULL);
 
@@ -130,9 +130,9 @@ DROP TABLE Shipment_Items cascade constraints;
 
 CREATE TABLE Shipment_Items (
 
-shipment_id NUMBER(3) NOT NULL,
+shipment_id NUMBER(9) NOT NULL,
 
-order_item_id NUMBER(3) NOT NULL,
+order_item_id NUMBER(9) NOT NULL,
 
 PRIMARY KEY(shipment_id,order_item_id));
 
@@ -144,7 +144,7 @@ DROP TABLE Ref_Invoice_Status_Codes cascade constraints;
 
 CREATE TABLE Ref_Invoice_Status_Codes (
 
-invoice_status_code NUMBER(3) PRIMARY KEY,
+invoice_status_code NUMBER(9) PRIMARY KEY,
 
 invoice_status_description CHAR(30) NOT NULL);
 
@@ -156,7 +156,7 @@ DROP TABLE Ref_Order_Item_Status_Codes cascade constraints;
 
 CREATE TABLE Ref_Order_Item_Status_Codes (
 
-order_item_status_code NUMBER(3) PRIMARY KEY,
+order_item_status_code NUMBER(9) PRIMARY KEY,
 
 order_item_status_description CHAR(30) NOT NULL);
 
@@ -168,11 +168,11 @@ DROP TABLE Orders cascade constraints;
 
 CREATE TABLE Orders (
 
-order_id NUMBER(3) PRIMARY KEY,
+order_id NUMBER(9) PRIMARY KEY,
 
-customer_id NUMBER(3) REFERENCES Customers (customer_id),
+customer_id NUMBER(9) REFERENCES Customers (customer_id),
 
-order_status_code NUMBER(3) REFERENCES Ref_Order_Status_Codes (order_status_code),
+order_status_code NUMBER(9) REFERENCES Ref_Order_Status_Codes (order_status_code),
 
 date_order_placed DATE NULL,
 
@@ -186,11 +186,11 @@ DROP TABLE Invoices cascade constraints;
 
 CREATE TABLE Invoices (
 
-invoice_number NUMBER(3) PRIMARY KEY,
+invoice_number NUMBER(9) PRIMARY KEY,
 
-order_id NUMBER(3) REFERENCES Orders (order_id),
+order_id NUMBER(9) REFERENCES Orders (order_id),
 
-invoice_status_code NUMBER(3) REFERENCES Ref_Invoice_Status_Codes (invoice_status_code),
+invoice_status_code NUMBER(9) REFERENCES Ref_Invoice_Status_Codes (invoice_status_code),
 
 invoice_date DATE NULL,
 
@@ -204,11 +204,11 @@ DROP TABLE Payments cascade constraints;
 
 CREATE TABLE Payments (
 
-payment_id NUMBER(3) PRIMARY KEY,
+payment_id NUMBER(9) PRIMARY KEY,
 
-invoice_number NUMBER(3) REFERENCES Invoices (invoice_number),
+invoice_number NUMBER(9) REFERENCES Invoices (invoice_number),
 
-payment_date DATE ,
+payment_date DATE,
 
 payment_amount NUMBER(20) NOT NULL);
 
@@ -220,15 +220,15 @@ DROP TABLE Order_Items cascade constraints;
 
 CREATE TABLE Order_Items (
 
-order_item_id NUMBER(3) PRIMARY KEY,
+order_item_id NUMBER(9) PRIMARY KEY,
 
-product_id NUMBER(3) REFERENCES Products (product_id),
+product_id NUMBER(9) REFERENCES Products (product_id),
 
-order_id NUMBER(3) REFERENCES Orders (order_id),
+order_id NUMBER(9) REFERENCES Orders (order_id),
 
-order_item_status_code NUMBER(3) REFERENCES Ref_Order_Status_Codes (order_status_code),
+order_item_status_code NUMBER(9) REFERENCES Ref_Order_Status_Codes (order_status_code),
 
-order_item_quantity NUMBER(1) NOT NULL,
+order_item_quantity NUMBER(3) NOT NULL,
 
 order_item_price NUMBER(3) NOT NULL,
 
@@ -248,14 +248,14 @@ DROP TABLE Shipments cascade constraints;
 
 CREATE TABLE Shipments (
 
-shipment_id NUMBER(3) PRIMARY KEY,
+shipment_id NUMBER(9) PRIMARY KEY,
 
-order_id NUMBER(3) REFERENCES Orders (order_id),
+order_id NUMBER(9) REFERENCES Orders (order_id),
 
-invoice_number NUMBER(3) REFERENCES Invoices (invoice_number),
+invoice_number NUMBER(9) REFERENCES Invoices (invoice_number),
 
 shipment_tracking_number NUMBER(20) NOT NULL,
 
-shipment_date DATE ,
+shipment_date DATE,
 
 other_shipment_details CHAR(40) );
